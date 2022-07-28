aec57b9bdc108b20c7094c8bb7a3206b31aedebf
prakhar-shukla-in <prakhars144@gmail.com>
2022-07-29 04:44:21 +0530

    Updates

---
+++ precache-manifest.016cfaa20bfd0f701af2f0881373f08f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7a47237f45190c4682428cfbcc3400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa54d7d5ae8b6f419ed",
+    "url": "/static/css/main.94be4470.chunk.css"
+  },
+  {
+    "revision": "a1f37604b6aad6ceb5ba",
+    "url": "/static/js/2.20c13233.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.20c13233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa54d7d5ae8b6f419ed",
+    "url": "/static/js/main.e0db838e.chunk.js"
+  },
+  {
+    "revision": "79b9e133eb2b5fe26530",
+    "url": "/static/js/runtime-main.03764f6f.js"
+  },
+  {
+    "revision": "a9c9e2888077d3a78965969daf497015",
+    "url": "/static/media/avatar.a9c9e288.png"
+  },
+  {
+    "revision": "189c94f9d4bad2fbd5727607d3e324ee",
+    "url": "/static/media/code-background.189c94f9.jpg"
+  },
+  {
+    "revision": "0d9d6556bdbd0e12fc6ad01f1772b7dc",
+    "url": "/static/media/project1.0d9d6556.png"
+  }
+]);
